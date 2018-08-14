4ad3caf5b659e8def705defaa1140c84e52a4e69
Aylin McGinnis <amcginnis@banyancom.com>
2018-08-14 14:26:06 -0400

    set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent }      from './dashboard/dashboard.component';
+import { TasksComponent }      from './tasks/tasks.component';
+import { ProjectsComponent }      from './projects/projects.component';
+
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: 'projects', component: ProjectsComponent },
+
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
