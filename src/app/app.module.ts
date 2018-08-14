1c74ade988618cd64c3ebae47d7e3c9af718e309
Aylin McGinnis <amcginnis@banyancom.com>
2018-08-14 14:12:41 -0400

    add component for projects and tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    DashboardComponent
+    DashboardComponent,
+    TasksComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
