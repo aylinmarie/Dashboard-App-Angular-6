7322c6f3a7a080e5b03cc96824308f6fba13cdd4
Aylin Marie <hello@aylinmarie.co>
2018-08-15 16:01:42 -0400

    update home navigatoin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { ProjectsComponent }      from './projects/projects.component';
 
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', component: DashboardComponent },
   { path: 'tasks', component: TasksComponent },
   { path: 'projects', component: ProjectsComponent },
 
