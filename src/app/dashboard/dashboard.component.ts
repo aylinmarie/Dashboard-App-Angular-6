129992d0ddfa564bdaf35319d9a4b2d8cf4cb627
Aylin Marie <hello@aylinmarie.co>
2018-08-16 10:48:24 -0400

    show list of projects in dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Project } from '../project';
+import { PROJECTS } from '../project-list';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  projects = PROJECTS;
 
   constructor() { }
 
